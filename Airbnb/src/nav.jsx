c1855b095dee6f6d5a9ae0c904e9c4cff0ad265a
Adnan056 <adnansohail1@yahoo.com>
2024-05-02 12:45:04 +0530

    Project

---
+++ Airbnb/src/nav.jsx
@@ -0,0 +1,19 @@
+function Nav(){
+    return(
+        <div>
+            <div className="Header">
+                <img src="./src/assets/airbnb-logo.png" alt="airbnb logo" />
+            </div>
+            <div className="context">
+                <div className="photogrid">
+                    <img src="./src/assets/photo-grid.png" alt="Image slide" /></div>
+                <div className="text">
+                    <h1>Online Experiences</h1>
+                    <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>     
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
